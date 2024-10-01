db643f10de85266b336086e249bc1ee2f299e23a
cowilliams_deloitte <cowilliams@deloitte.com>
2024-10-01 11:06:38 -0600

    paused on section 10

--- job-board-end-section-09/server/db/companies.js
+++ job-board-end-section-09/server/db/companies.js
@@ -1,7 +1,17 @@
+import Dataloader from 'dataloader';
 import { connection } from './connection.js';
+
 
 const getCompanyTable = () => connection.table('company');
 
 export async function getCompany(id) {
   return await getCompanyTable().first().where({ id });
+}
+
+export function createCompanyLoader() {
+return new Dataloader(async (ids) => {
+  console.log('[Company Loader] ids:',ids)
+  const companies = await getCompanyTable().select().whereIn('id', ids)
+  return ids.map(id => companies.find(company => company.id ===id))
+})
 }
